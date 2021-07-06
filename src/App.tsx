0ce9ed33a335079166da4af476b27fbef3eef26f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-06 23:44:47 +0600

    👑Icons are working Craprave

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import './App.css';
+
+const markerIcon = L.icon({
+	iconUrl: "https://avatars.githubusercontent.com/u/46283609?s=30&v=4"
+});
+
 
 function App() {
 	const position: any = [51.505, -0.09]
@@ -11,7 +17,7 @@ function App() {
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 				url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
 			/>
-			<Marker position={position}>
+			<Marker icon={markerIcon} position={position}>
 				<Popup>
 					A pretty CSS3 popup. <br /> Easily customizable.
 				</Popup>
