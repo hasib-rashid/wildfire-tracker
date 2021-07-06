df82902cf02e4ecced29343b16cc2e9d182f109b
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-07-06 23:40:24 +0600

    👑A huge Fix and It is WORKING!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 import './App.css';
 
 function App() {
+	const position: any = [51.505, -0.09]
 	return (
-		<main>
-			<h1>Hello World.</h1>
-		</main>
+		<MapContainer className="Map" center={position} zoom={2} scrollWheelZoom={true}>
+			<TileLayer
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+			/>
+			<Marker position={position}>
+				<Popup>
+					A pretty CSS3 popup. <br /> Easily customizable.
+				</Popup>
+			</Marker>
+		</MapContainer>
 	);
 }
 
